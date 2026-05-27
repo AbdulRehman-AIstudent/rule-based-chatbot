34225e8f866661f0ce0057d9c9d25e771192aba4
Abdul Rehman <myself.abdul098@gmail.com>
2026-05-27 07:18:51 +0500

    chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,55 @@
-print("___Start Chat___\n")
-a = input("You:")
+print("______________________ Chat with Max _______________________\n")
+print("Try asking:\n")
+print("___Hi")
+print("___Who are you?")
+print("___Who is your owner?")
+print("___What is your name?")
+print("___Write a script")
+print("___Thanks")
+print("___Bye")
 
-if a == "Hi".lower:
-    print("Max: Hello, How can I help you?")
-    
-   
+script = "[0:00 – Hook]\nTum AI use nahi kar rahe…” (pause)\nya AI tumhe replace karne wala hai?”\n[0:03]\n2026 me skill ka matlab badal chuka hai…”\n[0:05]\nAb hard work nahi…” (pause)\nsmart tools jeet rahe hain”\n[0:08]\n“Ek banda AI use karta hai…” (pause)\naur 1 ghante ka kaam 10 minute me karta hai”\n[0:12]\n“Dusra banda wahi kaam…” (pause)\n“pure din me karta rehta hai”\n[0:15]\n“Difference talent ka nahi…” (pause)\n“tools ka hai”\n[0:17]\n“AI tumhara dushman nahi hai…”\n[0:19]\n“Wo tumhara shortcut hai”\n[0:21]\n“Content banana ho…”\n“Study karni ho…”\n“Paise kamane ho…”\n[0:24 – Power Line]\n“AI use karo…” (pause)\n“warna ignore ho jao”\n[0:27 – CTA]\n“Agar tum AI master karna chahte ho…”\n“To abhi follow karo…”\n“Main tumhe wo AI tools sikhaunga…\nJo 90% logon ko pata hi nahi"
+
+while True:
+
+    a = input("\nYou: ").lower()  
+
+    if a == "hi":  
+        print("\nMax: Hello, How can I help you?")  
+  
+    elif a == "who are you?":  
+        print("\nMax: I am a chatbot. I can chat with you.")  
+      
+    elif a == "who is your owner?":  
+        print("\nMax: Abdul-Rehman is my owner.")  
+      
+    elif a == "what is your name?":  
+        print("\nMax: My name is Max Well.")  
+      
+    elif a == "write a script":  
+        print("\nMax: What type of script you want?\n\n1.Horror\n2.Emotional\n3.AI Motivational\n4.Moral\n5.Funny")  
+      
+    elif a == "ai motivation":  
+        print("\nMax:",script)  
+      
+    elif a == "horror":  
+        print("\nMax: Sorry, I am not able to write script on Horror.\nTry AI Motivation")  
+      
+    elif a == "emotional":  
+        print("\nMax: Sorry, I am not able to write script on Emotional.\nTry AI Motivation")  
+      
+    elif a == "moral":  
+        print("\nMax: Sorry, I am not able to write script on Moral.\nTry AI Motivation")  
+      
+    elif a == "funny":  
+        print("\nMax: Sorry, I am not able to write script on Funny.\nTry AI Motivation")  
+      
+    elif a == "thanks":  
+        print("\nMax: It's my duty")  
+      
+    elif a == "bye":  
+        print("\nMax: Good Bye, Take care")  
+        break  
+      
+    else:  
+          print("\nMax: Sorry, I don't understand")
